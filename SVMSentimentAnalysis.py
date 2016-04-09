a72d3a2431f99c39401c445e8b62f549f964c0dd
smeera380 <smeera380@ufl.edu>
2016-04-09 18:30:27 -0400

    Added files via upload

--- SVMSentimentAnalysis.py
+++ SVMSentimentAnalysis.py
@@ -1,5 +1,6 @@
 import cPickle,os
-import Preprocessor as pp
+import preProcessor as pp
+#from Preprocessor import transformData
 import numpy
 import re
 from keras.models import Sequential
@@ -24,10 +25,12 @@ class SVMSentiment:
        print('Configuring the SVM Model')
        currPath = os.getcwd()
        currFiles =  os.listdir(currPath)
+       print('################### Test #####################')
+       print(currFiles.count('SVMScores.pkl'))
        if(currFiles.count('SVMScores.pkl')==0):
           self.model.fit(TrainX, TrainY)
           # Saving model scores
-          joblib.dump(self.model,'SVMScores.pkl')
+          joblib.dump(self.model,currPath+'/SVMScores.pkl')
        else:
           print('Loading already existing Model')
           self.model = joblib.load(currPath+'/SVMScores.pkl')
@@ -38,12 +41,20 @@ class SVMSentiment:
 
        predicted_data=[]
        for i in range(len(TestX)):
-          predicted_data.append(list([self.model.predict(TestX[i].reshape(1,-1)),TestY[i]]))
+          predicted_data.append(list([self.model.predict (TestX[i].reshape(1,-1)) ,TestY[i]]) )
 
        print "Predicted Data"
        print predicted_data
        #print TestY
 
+    def predictSentiment(self,dataX,dataY):
+       print('@@@@@@@@@@@@@@@@ Length of test data : ',len(dataX))
+       for i in range(len(dataX)):
+         predicted_data = self.model.predict(dataX[i].reshape(1,-1))
+         expected_out = dataY[i]
+
+       print('############### Predicted data :',predicted_data,' ; ; ',expected_out)
+       return predicted_data
 
     def getTrainTestData(self):
        print('Loading Training and Test data')
@@ -51,95 +62,28 @@ class SVMSentiment:
        trainY=[]
        testX=[]
        testY = []
-
        f= open('trainingdata.pkl','rb')
-       (trainX,trainY) = cPickle.load(f)
-       
+       (trainX,trainY) = cPickle.load(f)       
        f= open('testingdata.pkl','rb')
        (testX,testY)  = cPickle.load(f)
 
        return ((trainX,trainY),(testX,testY))
 
 
-    def build_dict(self,trainX,testX):
-       sentences =[]
-       sentences = trainX + testX
-       wordCnt = dict()
-       # Splitting each sentences into words and getting the word count.
-       for i in sentences:
-         words = i.lower().split()
-         for w in words:
-            if w not in wordCnt:
-               wordCnt[w] = 1
-            else:
-               wordCnt[w] +=1
-
-       counts = wordCnt.values()
-       keys = wordCnt.keys()
-       worddict = dict()
-       sorted_idx = numpy.argsort(counts)[::-1]
-
-       for idx, ss in enumerate(sorted_idx):
-          worddict[keys[ss]] = idx+2
-
-       return worddict
-
-    # Transforms sentences into Integer vectors where number represents value corresponding to the word in the Dictionary built above
-    def transformData(self,dataX,dataY,worddict):
-       transformedDataX = [None] * len(dataX)
-       transformedDataY = dataY
-       for ind,sen in enumerate(dataX):
-          words = re.sub("[^\w]", " ", sen).lower().split()
-          transformedDataX[ind]=[]
-          for w in words:
-             if w in worddict:
-                transformedDataX[ind].append(worddict[w])
-             else:
-                transformedDataX[ind].append(1)
-          
-       #Converting the length of the transformed data to maximum length
-       transX = []
-       for i in transformedDataX:
-          transLen = len(i)
-          if(transLen < self.max_length): #Pad zeroes to the data vector
-              transX.append([0]*(self.max_length - transLen) + i)
-          elif transLen > self.max_length:
-              j = i
-              del j[self.max_length:]
-              transX.append(j)
-          #transformedData[ind] = [worddict[w] if w in worddict else 1 for w in words]
-   
-       transX = numpy.array(transX)
-       transformedDataY = numpy.array(transformedDataY)
-       transformedDataY = transformedDataY.reshape(transformedDataY.shape[0],1)
-
-       return (transX, transformedDataY)
-
-
-    def prepareData(self,dataX):
-       wordList=[]
-       for i in xrange(0,len(dataX)):
-          wordList[i] = re.sub("[^\w]", " ",  dataX[i]).split()
 
 
     def getValidationData(self,dataX,dataY):
        return dataX[0:self.batch_size,:],dataY[0:self.batch_size,:]
 
-
-def main():
+# arg rpresents the path of the filename which has the text converted from the speech signal.
+def main(arg):
    print('Initializing the LSTM Model')
+   cwd = os.getcwd()
    svm = SVMSentiment()
    
    print('Retrieving the Training and Test Data')
    path = os.getcwd()
-   ((trainX,trainY),(testX,testY)) = svm.getTrainTestData()
-
-   worddict = dict()
-   worddict = svm.build_dict(trainX,testX)
-
-   print('Transforming Training and Test Data')
-   (TrainX,TrainY) = svm.transformData(trainX,trainY,worddict)
-   (TestX,TestY) = svm.transformData(testX,testY,worddict)
+   ((TrainX,TrainY),(TestX,TestY)) = svm.getTrainTestData()
 
    print('Getting the Validation Data')
    validX, validY = svm.getValidationData(TrainX,TrainY)
@@ -147,9 +91,24 @@ def main():
    print('Configuring the SVM Model')
    svm.configureSVMModel(TrainX,TrainY,validX,validY)
 
-   print('Evaluating the Model')
+   #print('Evaluating the Model')
    svm.evaluateSVMModel(TestX,TestY)
+
+   if arg=='':
+      return
+   else:
+      emotfile = open(arg,"rb");
+      dataX = []
+      dataY =[]
+      dataX.append(emotfile.read())
+      dataY.append('0') #Random output so as to call the pp.transformData function. This is not to be used anywhere
+      worddict = cPickle.load(open(cwd+'/dictionary.pkl','rb'))
+      (DataX,DataY) = pp.transformData(dataX,dataY,worddict)       
+      prediction = svm.predictSentiment(DataX,DataY)
+      return prediction
+
 
    
 if __name__ =='__main__':
-   main()
+   arg ='/home/smeera380/spark-1.6.0/SpeechProject/SpeechSentimentAnalysis/aclImdb/emotion.txt'
+   main(arg)
---
+++ preProcessor.py
@@ -0,0 +1,140 @@
+import os
+import cPickle
+import numpy
+import re
+
+max_length  = 500
+
+def getData(datasetPath , dataType):
+    print('Test')
+    filepath = os.path.join(datasetPath,'train/')
+    files = glob.glob(filepath)
+    print(type(files),'    ',files)
+
+
+def train_test_split(datasetpath,datatype):
+    filePath = os.path.join(datasetpath,datatype+'/')
+    print('Filepath  ' , filePath)
+    sentences =[]
+    dataX=[]
+    dataY=[]
+
+    # Extracting the Positive sentiments
+    posFilePath = os.path.join(filePath,'pos/')
+    posFiles = os.listdir(posFilePath)
+    for i in posFiles:
+      if i.endswith('.txt') :
+         f = open(posFilePath+'/'+i,'r')
+         #sentences.append(f.read())
+         dataX.append(f.read())
+         dataY.append('1')
+         f.close()
+
+    # Extracting the Negative sentiments
+    negFilePath = os.path.join(filePath,'neg/')
+    negFiles = os.listdir(negFilePath)
+    for i in negFiles:
+      if i.endswith('.txt') :
+         g = open(negFilePath+'/'+i,'r')
+         #sentences.append(f.read())
+         dataX.append(g.read())
+         dataY.append('0')
+         g.close()
+
+    return (dataX,dataY)
+    
+  # This method builds a dictionary of words combining the training and test data which is used during the sentence to integer vector transformation
+def build_dict(trainX,testX):
+       sentences =[]
+       sentences = trainX + testX
+       wordCnt = dict()
+       # Splitting each sentences into words and getting the word count.
+       for i in sentences:
+         words = i.lower().split()
+         for w in words:
+            if w not in wordCnt:
+               wordCnt[w] = 1
+            else:
+               wordCnt[w] +=1
+
+       counts = wordCnt.values()
+       keys = wordCnt.keys()
+       worddict = dict()
+       sorted_idx = numpy.argsort(counts)[::-1]
+
+       for idx, ss in enumerate(sorted_idx):
+          worddict[keys[ss]] = idx+2
+    
+       return worddict
+
+  # Transforms sentences into Integer vectors where number represents value corresponding to the word in the Dictionary built above
+def transformData(dataX,dataY,worddict):
+       global max_length
+       transformedDataX = [None] * len(dataX)
+       transformedDataY = dataY
+       for ind,sen in enumerate(dataX):
+          words = re.sub("[^\w]", " ", sen).lower().split()
+          transformedDataX[ind]=[]
+          for w in words:
+             if w in worddict:
+                transformedDataX[ind].append(worddict[w])
+             else:
+                transformedDataX[ind].append(1)
+          
+       #Converting the length of the transformed data to maximum length
+       transX = []
+       for i in transformedDataX:
+          transLen = len(i)
+          if(transLen < max_length): #Pad zeroes to the data vector
+              transX.append([0]*(max_length - transLen) + i)
+          elif transLen > max_length:
+              j = i
+              del j[max_length:]
+              transX.append(j)
+          #transformedData[ind] = [worddict[w] if w in worddict else 1 for w in words]
+   
+       transX = numpy.array(transX)
+       transformedDataY = numpy.array(transformedDataY)
+       transformedDataY = transformedDataY.reshape(transformedDataY.shape[0],1)
+
+       return (transX, transformedDataY)
+
+
+def main():
+   #preProc = preProcessor()
+   path = os.getcwd() # This needs to be executed from within the DIRECTORY
+   
+   # Get the training data from the 'train' folder
+   print('Extracting the Training Data')
+   (trainX,trainY) = train_test_split(path,'train')
+
+   # Get the test data from the 'test' folder
+   print('Extracting the Test data')
+   (testX,testY)  = train_test_split(path,'test')
+
+   print('Building the dictionary')
+   worddict = dict()
+   worddict = build_dict(trainX,testX)
+
+   print('Transforming Training and Test Data')
+   (TrainX,TrainY) = transformData(trainX,trainY,worddict)
+   (TestX,TestY) = transformData(testX,testY,worddict)
+
+   # Dumping the dictionary to be used for external speech text input
+   dictDump = open("dictionary.pkl","wb")
+   print('Dumping the dictionary data')
+   cPickle.dump(worddict,dictDump,-1)
+
+   # Dumping the Training and Test data
+   trainDump = open("trainingdata.pkl","wb")
+   testDump = open("testingdata.pkl","wb")
+   print('Dumping the Training Data')
+   cPickle.dump((TrainX,TrainY),trainDump,-1)
+   trainDump.close()
+
+   print('Dumping the Test Data')
+   cPickle.dump((TestX,TestY),testDump,-1)
+   testDump.close()
+
+if __name__ =='__main__':
+   main()
