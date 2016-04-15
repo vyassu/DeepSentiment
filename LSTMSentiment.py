8c53172aaf101633542dd3f6adaf66facf6eeebe
smeera380 <smeera380@ufl.edu>
2016-04-14 21:47:52 -0400

    Added files via upload

--- LSTMSentiment.py
+++ LSTMSentiment.py
@@ -22,19 +22,20 @@ class LSTMSentiment:
        # Initializing a sequential Model
        self.model = Sequential()
        self.model.add(Embedding(max_features, 128, input_length=self.max_length))
+       self.model.add(Dropout(0.2))
        #self.model.add(LSTM(output_dim=128,input_dim=500,activation='relu'))
-       self.model.add(LSTM(output_dim=128))
+       self.model.add(LSTM(300))
 
-       self.model.add(Dropout(0.5))
+       self.model.add(Dropout(0.2))
        self.model.add(Dense(1))
-       self.model.add(Activation('softplus'))
+       self.model.add(Activation('linear'))
 
 
     def configureLSTMModel(self,TrainX,TrainY):
        print('Configuring the LSTM Model')
-       self.model.compile(loss='categorical_crossentropy', optimizer='adadelta', class_mode ="binary")
+       self.model.compile(loss='binary_crossentropy', optimizer='adam') #class_mode ="binary"
        #,class_mode ="binary")
-       self.model.fit(TrainX, TrainY, nb_epoch=12,batch_size=64, show_accuracy=True,validation_split=0.2)
+       self.model.fit(TrainX, TrainY, nb_epoch=15,batch_size=32, show_accuracy=True,validation_split=0.2)
        #,validation_data =(ValidX,ValidY))
 
 
@@ -242,7 +243,18 @@ def main():
 
    print('Evaluating the Model')
    lstm.evaluateLSTMModel(TestX,TestY)
-   
+
+   emotfile = open('/home/smeera380/spark-1.6.0/SpeechProject/SpeechSentimentAnalysis/aclImdb/emotion.txt',"rb");
+   dataX = []
+   dataY =[]
+   dataX.append(emotfile.read())
+   dataY.append('0') #Random output so as to call the pp.transformData function. This is not to be used anywhere
+   (DataX,DataY) = pp.transformData(dataX,dataY)
+   #print('+++++++++++++++++++++++++++++++++++')
+   #print(DataX, ': : ', DataY)
+   lstm.predictSentiment(DataX)
+
+
    
 if __name__ =='__main__':
    main()
