43dc71d73b7aec6643154a998185141f61142041
Vyas Kovakkat <vyas.kovakkat@ufl.edu>
2016-07-12 13:24:14 -0400

    File upload changes

--- deepui.js
+++ deepui.js
@@ -1,21 +1,17 @@
-navigator.getUserMedia  = navigator.getUserMedia ||
-                          navigator.webkitGetUserMedia ||
-                          navigator.mozGetUserMedia ||
+navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
                           navigator.msGetUserMedia;
 
-window.AudioContext = window.AudioContext ||
-                      window.webkitAudioContext;
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var analyser = context.createAnalyser();
 var oncounter = false;
-var array;
+var microphone_output_buffer;
+var i=1;
 
 var canvas = document.querySelector('.visualization');
 var canvasCtx = canvas.getContext("2d");
-
 WIDTH = canvas.width;
 HEIGHT = canvas.height;
-
 canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
 
@@ -24,22 +20,51 @@ function errorCallback(data)
 	console.log(data);
 }
 
-function recordSound()
-{	
-	console.log("in control");
-	if (oncounter==false)
-	{
-		context.resume();
-		navigator.getUserMedia({audio: true}, function(stream) {
-			var source = context.createMediaStreamSource(stream);     
-		    console.log(stream);             
-	  		source.connect(analyser);  
-	  		console.log(analyser)   ; 
-	  		analyser.connect(context.destination);
-	  		array = new Uint8Array(analyser.frequencyBinCount);
-            analyser.getByteFrequencyData(array);
-            console.log(array);
-            callwebservice(array);
+function process_microphone_buffer(event) {
+
+        
+		if(oncounter==true)
+		{
+			if (i==1)
+			{
+				microphone_output_buffer = new Float32Array(new ArrayBuffer(4096));
+				microphone_output_buffer.set(event.inputBuffer.getChannelData(0),0);
+			}
+			else
+			{
+				var temp_buffer = new Float32Array(new ArrayBuffer(4096*(i+1)));
+				temp_buffer.set(microphone_output_buffer,0);
+				temp_buffer.set(event.inputBuffer.getChannelData(0),microphone_output_buffer.length);
+				microphone_output_buffer = temp_buffer;
+				temp_buffer = null;
+			}
+			i++;
+			
+		}
+		else
+		{
+			microphone_output_buffer = new Float32Array(new ArrayBuffer(4096));
+			i=1;
+		}
+}
+
+function processTimeData(stream)
+{
+	gain_node = context.createGain();
+    gain_node.connect( context.destination );
+
+    microphone_stream = context.createMediaStreamSource(stream);
+    microphone_stream.connect(gain_node); 
+
+    script_processor_node = context.createScriptProcessor(1024, 1, 1);
+    script_processor_node.onaudioprocess = process_microphone_buffer;
+
+    microphone_stream.connect(script_processor_node);
+}
+
+
+	/*
+	//callwebservice(array);
 
         drawVisual = requestAnimationFrame(recordSound);
         canvasCtx.fillStyle = 'rgb(200, 200, 200)';
@@ -67,45 +92,97 @@ function recordSound()
 
      	canvasCtx.lineTo(canvas.width, canvas.height/2);
       	canvasCtx.stroke();
-		  }, errorCallback);
-		oncounter=true;
+		  */
+
+
+
+
+function recordSound()
+{	
+	if (oncounter==false)
+		{
+			context.resume();
+			navigator.getUserMedia({audio: true}, function(stream) {
+				processTimeData(stream);
+				oncounter = true;
+			}, errorCallback);
+		}
+		else
+		{
+			context.suspend();
+			callwebservice(false);
+			i=1;
+			oncounter = false;
+			microphone_output_buffer = new Float32Array(new ArrayBuffer(4096));
+		}
+}
+
+var filename;
+$("#filedata").on("change", function(event){
+	filename = event.target.files;
+}
+);
+
+function sendData()
+{
+	var filedata = new FileReader();
+    $.each(filename, function(index,file)
+    {
+    	filedata.readAsDataURL(file);
+    	filedata.onloadend = function(event)
+    	{
+    		console.log(filedata.result);
+    		microphone_output_buffer = filedata.result;
+    		callwebservice(true);
+    	}
+    });
+}
+
+
+function fillText(data)
+{
+
+}
+
+function callwebservice(fileupload)
+{
+	var webdata,URL;
+	if(fileupload==false)
+	{
+		var soundData="["+microphone_output_buffer.join();
+		soundData = soundData+"]";
+		webData = {
+						"data" :soundData,
+						"samplerate": context.sampleRate
+					  }
+		webdata =  JSON.stringify(webData);
+		URL= "http://localhost:5000/deepsentimentweb"
 	}
 	else
 	{
-		context.suspend();
-		oncounter=false;
+		webdata = microphone_output_buffer;
+		URL = "http://localhost:5000/deepsentifile";
 	}
-}
-
-function callwebservice(var_data)
-{
-	var soundData="[";
-	for(var i=0; i < var_data.length-1;i++)
-		{
-			soundData = soundData+var_data[i]+",";
-			//console.log(var_data[i]);
-		}
-
-	var webData = {
-					"data" :soundData = soundData+var_data[var_data.length-1]+"]",
-					"samplerate": context.sampleRate
-				  }
 
 	$.ajax({
             type: "POST",
             method: "POST",
-            url: "http://localhost:5000/deepsentimentweb",
+            url: URL,
             contentType: "application/json",
             datatype:"text",
-            data: JSON.stringify(webData),
+            data: webdata,
             timeout : 5000,
             success : function(data)
             {
-            	console.log(data);
+            	canvasCtx.font = "24px Times New Roman";
+            	canvasCtx.fillStyle = "white";
+				canvasCtx.fillText("What you spoke:: "+data.input,10,50);
+				canvasCtx.fillText(data.result,10,90);
             },
             error: function(data)
             {
             	
+            	alert("Backend server not responding!! Please try again after sometime");
             	console.log(data);
             }
         });
