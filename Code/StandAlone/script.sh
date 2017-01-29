b0d82fd7aa2e13945ac3d1d3ab03b2f2e982bd6f
Vyas Kovakkat <vyas.kovakkat@ufl.edu>
2017-01-29 11:31:24 -0500

    Script file upload

---
+++ Code/StandAlone/script.sh
@@ -0,0 +1,13 @@
+if [ ! -d "./Data" ]
+then
+	mkdir Data
+fi
+if [ ! -d "./inputdata" ]
+then
+	mkdir inputdata
+fi
+
+cp - R ../../WebInterface/* .
+
+python ./webservice.py
+echo "Webservice started!! Use URL http://localhost:5000/deepsentiment"
